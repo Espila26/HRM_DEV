b0b3a040ab516289db28cf3dbab393936d8d1017
Esteban Picado <epicado2010@hotmail.com>
2017-03-26 19:54:36 -0600

    Add project files.

---
+++ HRM_DEV/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HRM_DEV.MvcApplication" Language="C#" %>
---
+++ HRM_DEV/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HRM_DEV.Startup))]
+namespace HRM_DEV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
