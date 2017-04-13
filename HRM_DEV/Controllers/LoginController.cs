3875d86b9c1125b599e502318ac12ea0e397b1b9
JordyRG21 <jordyromgu@hotmail.com>
2017-04-13 17:47:47 -0600

    Login

---
+++ HRM_DEV/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using HRM_DEV.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRM_DEV.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private hrm_dbEntities db = new hrm_dbEntities();
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Cuenta(string NOMBRE_USUARIO, string CONTRASEÑA)
+        {
+            var usuarios = from e in db.USUARIOS
+                               select e;
+
+            usuarios = usuarios.Where(s => (s.NOMBRE_USUARIO.Equals(NOMBRE_USUARIO)));
+
+            if (usuarios != null)
+            {
+                if (usuarios.First().CONTRASEÑA.Equals(CONTRASEÑA))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
